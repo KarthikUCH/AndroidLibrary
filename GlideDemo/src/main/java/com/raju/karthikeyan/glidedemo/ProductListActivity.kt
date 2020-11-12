bed8fec8f8b65c453ecbec2bedda56511e169fb9
Karthi Keyan <karthikeyan027@gmail.com>
2020-11-12 11:52:39 +0530

    All Glide dependency both to GildeDemo module and app module

--- GlideDemo/src/main/java/com/raju/karthikeyan/glidedemo/ProductListActivity.kt
+++ GlideDemo/src/main/java/com/raju/karthikeyan/glidedemo/ProductListActivity.kt
@@ -2,10 +2,19 @@ package com.raju.karthikeyan.glidedemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import com.raju.karthikeyan.glidedemo.glide.GlideApp
+import java.net.URL
 
 class ProductListActivity : AppCompatActivity() {
+    lateinit var imageView: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_product_list)
+        imageView = findViewById(R.id.imageView)
+
+        GlideApp.with(this)
+            .load("https://picsum.photos/200")
+            .into(imageView);
     }
 }
--- app/src/main/java/com/raju/karthikeyan/demoglide/MainActivity.kt
+++ app/src/main/java/com/raju/karthikeyan/demoglide/MainActivity.kt
@@ -4,11 +4,14 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import com.raju.karthikeyan.glidedemo.ProductListActivity
+import com.raju.karthikeyan.glidedemo.glide.GlideApp
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var btnOpen: Button
+    lateinit var imageView: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -17,5 +20,10 @@ class MainActivity : AppCompatActivity() {
         btnOpen.setOnClickListener {
             startActivity(Intent(this, ProductListActivity::class.java))
         }
+
+        imageView = findViewById(com.raju.karthikeyan.glidedemo.R.id.imageView)
+        GlideApp.with(this)
+            .load("https://picsum.photos/200")
+            .into(imageView);
     }
 }
